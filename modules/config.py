616c19a3302d5633d27b762033734ffe7b6f1483
Skorh20 <TheBossSkorh@gmail.com>
2024-10-22 20:55:20 +0300

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -8,10 +8,10 @@ que = {}
 admins = {}
 aiohttpsession = aiohttp.ClientSession()
 
-API_ID = int(getenv("API_ID", "9305835"))
-API_HASH = getenv("API_HASH", "a60c62f35b0db984f6fb6b9421e805d3")
-BOT_TOKEN = getenv("BOT_TOKEN", "5789746319:AAEEUCuPjS3cdwm3Rp0GpQMXq0XNfQDlYBQ")
+API_ID = int(getenv("API_ID", "21591773"))
+API_HASH = getenv("API_HASH", "7e59ca50130d93574ec3145e692430b8")
+BOT_TOKEN = getenv("BOT_TOKEN", "7893194449:AAEbKOe2ay8FtzIkF7oapXtwV1ybj6Wrz2k")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
-STRING_SESSION = getenv("STRING_SESSION", "BADCzxDXwy8ZixpjjiF7Ta5CRUczsYNl8OmmKv-FFdagx2zgcm6OjDN7PlB4mkrvWBVDtJzFFQPD2gqjsn2UFFomRI0uF7qsvoHSzk8n1KJpViER8sAxnfaPCHCR6fsRnebfhPBf-JYU8SX0EB7h-V3nGmfz57KhyXSfjCqf5ZqVOQnUxyOk8nGQtTgBv-w4XU-RmAzody7Ya7VBxDWxKkddj0kw1oVMg6Jv2VI_1zmgaunU7IaMyQMnYqzXokXa0ANNtsmu3u1i5YalSyHs3OzUo6aUbaXMVo_wZRPTSXx1h8pQN6vsocIxoMGwMrpQn2PrSqaCRsar5rEFI0rfz2EgAAAAAVdVw7kA")
+STRING_SESSION = getenv("STRING_SESSION", "AgFJdt0ApXYqZ8RDVv-yHrPKUCFzLUsaMO5H-UOp6iTRvs8djjmYao-aXBzA9ZF6WcUIXET-4WxVfK1hH-lTIbnH7VPb2_0a4tQNLs44bYbCX3-c89sFcut5e8859XEVN0OyI5h-FjNBMTtbAnxP7cb6JUjItjonxWtb6bdmGrAxpf16M68uwKoTKvrOjFPPPv_yoDgyahk13Vskq6Bd5D0Bnfl9sHBxmABVB3fzC8MAeHTjsrXRrbq14qr1bnQstUJuv7QKME-8Zs9iTkZWsN9aKawWmff239JS3Nb876pxZK4_xIf3bfNBrTbQiOzo5SzzdZkSHmMth5txV7iwkTm9AYvR_gAAAAG8GOfFAA")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7450716101").split()))
